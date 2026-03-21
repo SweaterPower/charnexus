2a01762c5a2db704c74d34749c26c87fb890afca
andrey.dubakov <andrey.dubakov.dev@gmail.com>
2026-03-21 20:47:34 +0500

    initial commit

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/profile', name: 'profile_')]
+class ProfileController extends AbstractController
+{
+    #[Route('/overview', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('profile/index.html.twig');
+    }
+}
